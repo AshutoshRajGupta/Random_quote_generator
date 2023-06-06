2f50d7017896878166593fa61fa83299f4985531
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-06-06 22:47:57 +0530

    code updated

---
+++ advice/src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import "./App.css";
+
+const App = () => {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        setAdvice(advice);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <h1 className="title">Advice Of The Day</h1>
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
